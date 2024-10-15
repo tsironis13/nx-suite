3141b330cd9612db34cbfc5d7d9cfa0597c46221
GIANNIS\tsiro <tsiro1@hotmail.com>
2024-10-15 13:31:02 +0300

    trpc config

--- libs/shared/domain/trpc-client/src/lib/trpc-client.ts
+++ libs/shared/domain/trpc-client/src/lib/trpc-client.ts
@@ -1,12 +1,30 @@
 import { createTrpcClient } from '@analogjs/trpc';
 import { inject } from '@angular/core';
 import type { AppRouter } from '@nx-suite/shared/domain/trpc-server';
+import { httpBatchLink } from '@trpc/client';
 
 export const { provideTrpcClient, TrpcClient, TrpcHeaders } =
   createTrpcClient<AppRouter>({
-    url: '/api/trpc',
-  });
+    url: `/api/trpc`,
+    options:             {
+      links: [httpBatchLink({
+        url: getBaseUrl() + '/api/trpc'
+      })]
+    }
+  });  
 
 export function injectTrpcClient() {
   return inject(TrpcClient);
+}
+
+function getBaseUrl() {
+  if (typeof window !== 'undefined') {
+    return '';
+  }
+
+  if (process.env['VERCEL_URL']) {
+    return `https://${process.env['VERCEL_URL']}`;
+  }
+
+  return `http://localhost:${process.env['PORT'] ?? 3000}`;
 }
