fda698b6f8433d7483b6a1083737a2e08efc0d65
GIANNIS\tsiro <tsiro1@hotmail.com>
2024-10-16 15:44:27 +0300

    connect db

--- libs/shared/domain/trpc-server/src/lib/trpc/routers/test.ts
+++ libs/shared/domain/trpc-server/src/lib/trpc/routers/test.ts
@@ -1,13 +1,21 @@
+import { db } from '../../drizzle/db';
+import { productCategories } from '../../drizzle/schema';
 import { publicProcedure, router } from '../trpc';
 
 export const testRouter = router({
-  getAll: publicProcedure.query(() => {
+  getAll: publicProcedure.query(async () => {
+    const x = await db.select().from(productCategories).limit(5);
+
     return {
       totalCount: 45,
-      items: [
-        { id: 1, name: 'dd', description: 'dfkjdfjk' },
-        { id: 2, name: 'ss2', description: 'dfkjdfjk123' },
-      ],
+      items: x,
     };
+    // return {
+    //   totalCount: 45,
+    //   items: [
+    //     { id: 1, name: 'dd', description: 'dfkjdfjk' },
+    //     { id: 2, name: 'ss2', description: 'dfkjdfjk123' },
+    //   ],
+    // };
   }),
 });
