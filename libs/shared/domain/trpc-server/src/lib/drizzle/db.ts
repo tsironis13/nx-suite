689d4312978f79d654100efc2dc51a47a54bf066
GIANNIS\tsiro <tsiro1@hotmail.com>
2024-10-16 15:54:09 +0300

    db config

--- libs/shared/domain/trpc-server/src/lib/drizzle/db.ts
+++ libs/shared/domain/trpc-server/src/lib/drizzle/db.ts
@@ -2,5 +2,8 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
 
-const client = postgres(process.env['DATABASE_URL'] ?? '');
+//const client = postgres(process.env['DATABASE_URL'] ?? '');
+const client = postgres(
+  'postgresql://postgres.smrapzkhjrhqwrserfao:YzfCwQunykpicAvE@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
+);
 export const db = drizzle(client, { schema, logger: true });
